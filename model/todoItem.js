8fe481017e7a1d7b2d85a2ca60071dded0894402
Aida Ospanova <ospanovaida08@gmail.com>
2019-01-31 09:42:56 +0600

    Update todoItem.js
    fixed "delete" bug

--- model/todoItem.js
+++ model/todoItem.js
@@ -19,6 +19,7 @@ todoItem.prototype.finish = function() {
 todoItem.prototype.remove = function() {
   if (this.container.list.indexOf(this) != -1) {
     this.container.list.splice(this.container.list.indexOf(this), 1);
+    this.container.saveToLS();
   }
 }
 
